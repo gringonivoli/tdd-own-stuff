684bb61808346fb284e8ba05f3ebf56b5da3c2a2
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2021-02-27 21:17:42 -0300

    :hammer: refactor (prime_numbers): Delete a big if/else

--- prime_numbers/int.py
+++ prime_numbers/int.py
@@ -1,12 +1,10 @@
 class Int(int):
 
     def is_prime(self):
-        result = True
-        if self == 1:
-            result = False
-        else:
-            for number in range(1, self):
-                if number != 1 and number != self and self % number == 0:
-                    result = False
-                    break
+        result = False if self <= 1 else True
+        for number in range(1, self):
+            if number != 1 and number != self and self % number == 0:
+                result = False
+                break
+
         return result
