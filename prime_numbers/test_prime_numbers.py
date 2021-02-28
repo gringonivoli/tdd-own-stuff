06fbbb3ee9f6593c384774ad25ede850454b2e06
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2021-02-27 21:29:05 -0300

    :white_check_mark: test (prime_numbers): Add and refactor tests

--- prime_numbers/test_prime_numbers.py
+++ prime_numbers/test_prime_numbers.py
@@ -2,24 +2,25 @@ from prime_numbers.prime_numbers import PrimeNumbers
 
 
 def test_prime_numbers():
-    prime_numbers = PrimeNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+    prime_numbers = PrimeNumbers([1, 2, 3, 4, 12, 13])
 
-    assert prime_numbers
+    assert prime_numbers is not None
 
 
-def test_prime_numbers_is_empty():
-    expected_value = []
+def test_prime_numbers_is_empty_for_no_values():
     prime_numbers = PrimeNumbers([])
 
-    result = [prime_number for prime_number in prime_numbers]
-
-    assert result == expected_value
+    assert list(prime_numbers) == []
 
 
-def test_prime_numbers_is_a_list_of_prime_numbers():
-    expected_value = [2, 3, 5, 7, 13, 11]
-    prime_numbers = PrimeNumbers([1, 2, 3, 4, 5, 6, 8, 7, 13, 11, 20, 21])
+def test_prime_numbers_is_empty_for_no_prime_numbers():
+    prime_numbers = PrimeNumbers([4, 6, 8, 10])
 
-    result = [prime_number for prime_number in prime_numbers]
+    assert list(prime_numbers) == []
 
-    assert result == expected_value
+
+def test_prime_numbers_is_a_iterable_of_prime_numbers():
+    expected_value = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    prime_numbers = PrimeNumbers(list(range(1, 100)))
+
+    assert list(prime_numbers) == expected_value
